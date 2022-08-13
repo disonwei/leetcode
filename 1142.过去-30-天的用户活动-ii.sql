f1c8288f0b6eda27c7b49d0a1085dd727b681dcf
dison <709142365@qq.com>
2022-08-13 12:23:51 +0800

    feat(1142): 30天用户活动ii [sql][datediff][round][ifnull]

---
+++ 1142.过去-30-天的用户活动-ii.sql
@@ -0,0 +1,110 @@
+--
+-- @lc app=leetcode.cn id=1142 lang=mysql
+--
+-- [1142] 过去30天的用户活动 II
+--
+-- https://leetcode.cn/problems/user-activity-for-the-past-30-days-ii/description/
+--
+-- database
+-- Easy (37.21%)
+-- Likes:    21
+-- Dislikes: 0
+-- Total Accepted:    10.5K
+-- Total Submissions: 28.2K
+-- Testcase Example:  '{"headers":{"Activity":["user_id","session_id","activity_date","activity_type"]},"rows":{"Activity":[[1,1,"2019-07-20","open_session"],[1,1,"2019-07-20","scroll_down"],[1,1,"2019-07-20","end_session"],[2,4,"2019-07-20","open_session"],[2,4,"2019-07-21","send_message"],[2,4,"2019-07-21","end_session"],[3,2,"2019-07-21","open_session"],[3,2,"2019-07-21","send_message"],[3,2,"2019-07-21","end_session"],[3,5,"2019-07-21","open_session"],[3,5,"2019-07-21","scroll_down"],[3,5,"2019-07-21","end_session"],[4,3,"2019-06-25","open_session"],[4,3,"2019-06-25","end_session"]]}}'
+--
+-- Activity 表：
+-- 
+-- 
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | session_id    | int     |
+-- | activity_date | date    |
+-- | activity_type | enum    |
+-- +---------------+---------+
+-- 该表没有主键，它可能有重复的行。
+-- activity_type 列是 ENUM 类型，可以取（“ open_session”，“ end_session”，“ scroll_down”，“
+-- send_message”）四种活动类型之一。
+-- 该表显示了社交媒体网站的用户活动。
+-- 请注意，每个会话只属于一个用户。
+-- 
+-- 
+-- 
+-- 编写 SQL 查询以查找截至 2019-07-27（含）的 30
+-- 天内每个用户的平均会话数，四舍五入到小数点后两位。只统计那些会话期间用户至少进行一项活动的有效会话。
+-- 
+-- 查询结果格式如下例所示。
+-- 
+-- 
+-- 
+-- 示例：
+-- 
+-- 
+-- 输入：
+-- Activity 表：
+-- +---------+------------+---------------+---------------+
+-- | user_id | session_id | activity_date | activity_type |
+-- +---------+------------+---------------+---------------+
+-- | 1       | 1          | 2019-07-20    | open_session  |
+-- | 1       | 1          | 2019-07-20    | scroll_down   |
+-- | 1       | 1          | 2019-07-20    | end_session   |
+-- | 2       | 4          | 2019-07-20    | open_session  |
+-- | 2       | 4          | 2019-07-21    | send_message  |
+-- | 2       | 4          | 2019-07-21    | end_session   |
+-- | 3       | 2          | 2019-07-21    | open_session  |
+-- | 3       | 2          | 2019-07-21    | send_message  |
+-- | 3       | 2          | 2019-07-21    | end_session   |
+-- | 3       | 5          | 2019-07-21    | open_session  |
+-- | 3       | 5          | 2019-07-21    | scroll_down   |
+-- | 3       | 5          | 2019-07-21    | end_session   |
+-- | 4       | 3          | 2019-06-25    | open_session  |
+-- | 4       | 3          | 2019-06-25    | end_session   |
+-- +---------+------------+---------------+---------------+
+-- 输出：
+-- +---------------------------+ 
+-- | average_sessions_per_user |
+-- +---------------------------+ 
+-- | 1.33                      |
+-- +---------------------------+
+-- 解释：用户 1 和 2 每人在过去 30 天有 1 个会话，而用户 3 有 2 个会话。所以平均是 (1 + 1 + 2) / 3 = 1.33 。
+-- 
+-- 
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+
+-- select round(avg(count(session_id)),2) as average_sessions_per_user
+-- select user_id,count(distinct session_id) as average_sessions_per_user
+-- from Activity
+-- where datediff("2019-07-27",activity_date)<30 and datediff("2019-07-27",activity_date)>0
+-- group by user_id
+
+
+
+
+-- select ifnull(round(sum(a1.average_sessions)/count(a1.user_id),2),0.0)  as average_sessions_per_user
+-- from (
+--     select user_id,count(distinct session_id) as average_sessions
+-- from Activity
+-- where datediff("2019-07-27",activity_date)<30 and datediff("2019-07-27",activity_date)>0
+-- group by user_id
+-- ) as a1
+
+
+SELECT IFNULL(ROUND(COUNT(DISTINCT session_id) / COUNT(DISTINCT user_id), 2), 0) AS average_sessions_per_user
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) < 30
+-- WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+
+
+-- @lc code=end
+/*
+round(x,d) ： 四舍五入x的d位小数
+ifnull(x1,x2) ： 如果x1为null 返回x2
+
+
+
+*/
